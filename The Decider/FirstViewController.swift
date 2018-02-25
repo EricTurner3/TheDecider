0426b103ba861f85fc22769a44b462b54ec541ea
Eric Turner <turnerec@iupui.edu>
2018-02-25 14:31:05 -0500

    Initial Commit

---
+++ The Decider/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  The Decider
+//
+//  Created by Eric Turner on 2/25/18.
+//  Copyright © 2018 Eric Turner. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
